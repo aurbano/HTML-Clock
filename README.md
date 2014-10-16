ef797d96b942fdb0d0f9d7a46949ea8cc4a20803
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2014-10-16 23:02:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+#HTML Clock
+
+> Display an HTML rendered functioning clock. Probably useless, but it was interesting to code.
+
+##Installation
+Download the source code and include the files in the `src` folder (css and js). You can obviously customize the css to your needs.
+
+It requires jQuery to work, so make sure it's included before the `html-clock.js` script.
+
+##Usage
+Add the following HTML, the `.clock` container is optional, you can have the clock occupy the full screen if you want.
+
+```html
+<div class="clock">
+  <div class="htmlClock" id="clock1">
+    <div class="htmlClock-visor"></div>
+    <div class="htmlClock-view hourView"></div>
+    <div class="htmlClock-view minuteView"></div>
+    <div class="htmlClock-view secondView"></div>
+  </div>
+</div>
+```
+
+Now add the following script:
+
+```javascript
+$(document).ready(function(){
+  var clock = new HTMLClock();
+
+  clock.init({
+    container: $('#clock1')
+  });
+});
+```
+
+##Settings
+The `init` method takes one argument that is the settings used. You can set the following:
+
+| Parameter   |      Values      |  Description |
+|-------------|------------------|--------------|
+| container   | $('#element')    | jQuery element where the clock should appear |
+
+##Roadmap
+The HTML clock is in a very early version, I have only spent a couple of hours on it so far, so lots of improvements need to be made. Feel free to create an issue if you spot anything that could be improved.
+
+Future development:
+* Adjustable size: Probably with a setting that will multiply all the sizes.
+* Themes: Add a couple of css files that will change the appearance of the clock.
+* Responsive: Similar to the adjustable size, but adapting to the container. It kind of works already, except for the fonts.
+
+##License
+The HTML Clock is released under the GNU GPLv2 License.
---
+++ src/html-clock.js
@@ -0,0 +1,135 @@
+var HTMLClock = function () {
+	var viewport = {
+			h: 0,
+			w: 0
+		},
+		block = {
+			w: 200
+		},
+		moveInterval = null,
+		// Default settings
+		settings = {
+			container: null
+		}
+
+	var clock = {};
+
+	clock.init = function (options) {
+		console.info("HTML-Clock started");
+		// Merge settings
+		settings = $.extend(settings, options);
+
+		// Adjust size
+		clock.adjust();
+
+		// Setup the HTML for the clock
+		clock.stripe.create('hour', 'next');
+		clock.stripe.create('minute', 'next');
+		clock.stripe.create('second', 'next');
+
+		clock.stripe.create('hour', 'current');
+		clock.stripe.create('minute', 'current');
+		clock.stripe.create('second', 'current');
+
+		moveInterval = setInterval(function () {
+			clock.stripe.move();
+		}, 1000 / 60);
+	};
+
+	clock.adjust = function () {
+		// Adjust all sizes
+		viewport.h = settings.container.height();
+		viewport.w = settings.container.width();
+
+		// Center visors
+		var center = viewport.w / 2,
+			hourLeft = center - block.w / 2 - block.w;
+
+		settings.container.find('.hourView').css({
+			left: hourLeft,
+			width: block.w
+		});
+
+		settings.container.find('.minuteView').css({
+			left: hourLeft + block.w,
+			width: block.w
+		});
+
+		settings.container.find('.secondView').css({
+			left: hourLeft + block.w * 2,
+			width: block.w
+		});
+
+		settings.container.css({
+			height: viewport.h + 'px',
+			width: viewport.w + 'px'
+		});
+
+		settings.container.find('.htmlClock-visor').css({
+			top: viewport.h / 2 - block.w / 2,
+			height: block.w
+		});
+	};
+
+	clock.stripe = {
+
+		hour: 4801,
+
+		minute: 12001,
+
+		create: function (type, which) {
+			// Creates a new number stripe up to num
+			var draw = '<div class="htmlClock-stripe ' + type + '_' + which + '">';
+			var num = 60;
+			if (type == 'hour') num = 24;
+			for (var i = 0; i < num; i++) {
+				if (i < 10) i = '0' + i;
+				draw += '<div class="htmlClock-numBlock">' + i + '</div>';
+			}
+			draw += '</div>';
+			settings.container.find('.' + type + 'View').append(draw);
+		},
+
+		move: function () {
+			// Calculates each position and apply
+			// I want it continuous, so I'll use all digits
+			var d = new Date();
+			var ms = d.getMilliseconds();
+			var s = d.getSeconds() + ms / 1000;
+			var m = d.getMinutes() + s / 60;
+			var h = d.getHours();
+
+			// Current offset
+			var curOffset = viewport.h / 2 - block.w / 2;
+
+			// Calculate offset
+			var hourOffset = -h * block.w + curOffset;
+			var minuteOffset = -m * block.w + curOffset;
+			var secondOffset = -s * block.w + curOffset;
+
+			// Apply to current and next
+			settings.container.find('.hour_current').css({
+				top: hourOffset
+			});
+			settings.container.find('.hour_next').css({
+				top: this.hour + hourOffset
+			});
+
+			settings.container.find('.minute_current').css({
+				top: minuteOffset
+			});
+			settings.container.find('.minute_next').css({
+				top: this.minute + minuteOffset
+			});
+
+			settings.container.find('.second_current').css({
+				top: secondOffset
+			});
+			settings.container.find('.second_next').css({
+				top: this.minute + secondOffset
+			});
+		}
+	};
+
+	return clock;
+};
